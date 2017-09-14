97d09393ca85ec426cf450e3d4e7296d65ffff23
Steven Broad <sebroad@yahoo.com>
2017-09-13 21:00:51 -0700

    Also download the results

--- Connect Server/launch.py
+++ Connect Server/launch.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Launch a PLEXOS Connect run
+
 Created on Mon Jun 05 11:36:46 2017
 
 @author: Steven
@@ -37,30 +39,54 @@ if len(jobset) == 0:
 cxn = plx.PLEXOSConnect()
 cxn.Connection('Data Source={};User Id={};Password={}'.format(server,username,password))
 
+# verify that the dataset exists
 if cxn.CheckDatasetExists(folder,dataset):
     
+    print
+    print 'Select the dataset version'
+    # List out the versions
     for v in cxn.GetDatasetVersions(folder,dataset):
         print v
         
-    version = raw_input('Which version? ')
+    version = raw_input('Which version? (Enter for latest)')
     
+    print
+    print 'Select the .xml database'
     files = cxn.GetDatasetFiles(folder,dataset,version)
     
+    # list out all of the files
     for (idx, f) in zip(range(len(files)),files):
         print idx + 1, '--->', f
         
     file_num = int(raw_input('Which file? (Enter the number): '))
     
+    # prompt user to select a Model
     xml_file = files[file_num - 1]
-    model = raw_input('Model:    ')
+    model = raw_input('\nModel: (in this example you need to know the name) ')
     
+    # Remove the jobset as needed
     if cxn.CheckJobsetExists(jobset):
         cxn.RemoveJobset(jobset)
 
+    # set up the arguments for the job
     args = ['"{}" -m "{}"'.format(xml_file, model)]
+    
+    # create a job set and add a job to it
     if cxn.AddJobset(jobset):
+        
+        # add the job and run it
         job = cxn.AddJob(jobset, folder, dataset, version, args, '', '', 0)
         run_id = cxn.AddRun(job)
+        
+        # track the progress of the run
+        while not cxn.IsRunComplete(run_id):
+            print cxn.GetRunProgress(run_id)
+        
+        # Download the result
+        cxn.DownloadSolution('.', run_id)
+        print 'Run', run_id, 'is complete.'
+        
+        
     else:
         print 'Jobset', jobset, 'not created.'
         
