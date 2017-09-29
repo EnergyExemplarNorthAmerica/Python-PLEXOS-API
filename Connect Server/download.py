3532eb671c38904c9e47441af68b7b53802c4588
Steven Broad <sebroad@yahoo.com>
2017-09-29 12:00:45 -0700

    More connect server

---
+++ Connect Server/download.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Launch a PLEXOS Connect run
+
+Created on Mon Jun 05 11:36:46 2017
+
+@author: Steven
+"""
+
+# standard Python/SciPy libraries
+import getpass, sys, os
+
+# Python .NET interface
+if not 'dotnet.seamless' in sys.modules:
+    from dotnet.seamless import add_assemblies, load_assembly
+
+
+# .NET related imports
+if not 'PLEXOS7_NET.Core' in sys.modules:
+    # load PLEXOS assemblies
+    add_assemblies('C:/Program Files (x86)/Energy Exemplar/PLEXOS 7.4/')
+    load_assembly('PLEXOS7_NET.Core')
+    import PLEXOS7_NET.Core as plx
+
+if not 'EEUTILITY.Enums' in sys.modules:
+    # load PLEXOS assemblies
+    add_assemblies('C:/Program Files (x86)/Energy Exemplar/PLEXOS 7.4/')
+    load_assembly('EEUTILITY')
+    from EEUTILITY.Enums import *
+
+if not 'System' in sys.modules:
+    from System import *
+    
+if not 'System.IO' in sys.modules:
+    from System.IO import SearchOption
+
+server = raw_input('Server:   ')
+username = raw_input('Username: ')
+password = getpass.getpass('Password: ')
+folder = raw_input('Folder:   ')
+dataset = raw_input('Dataset:  ')
+
+if len(dataset) == 0:
+    dataset = 'testdb'
+    
+# connect to the PLEXOS Connect server
+cxn = plx.PLEXOSConnect()
+cxn.Connection('Data Source={};User Id={};Password={}'.format(server,username,password))
+
+# verify that the dataset exists
+if not cxn.CheckDatasetExists(folder,dataset):
+    print 'Dataset', dataset, 'does not exist in folder', folder
+    
+else:
+    '''
+    String GetDatasetLatestVersion(
+    	String strFolderPath,
+    	String strDatasetName
+    	)
+    '''
+    ver = cxn.GetDatasetLatestVersion(folder,dataset)
+    
+    dest = os.path.join(folder,dataset)
+    os.mkdir(dest)
+
+    '''
+    Boolean DownloadDatasetVersion(
+    	String strDestination,
+    	String strFolderPath,
+    	String strDatasetName,
+    	String strVersion
+    	)
+    '''
+    cxn.DownloadDatasetVersion(dest, folder, dataset, ver)
+    
+    
