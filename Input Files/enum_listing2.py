cba5840b7bf94daa59871d121fda4ab45dc6a948
Steven Broad <steven.broad@energyexemplar.com>
2018-01-31 08:22:02 -0800

    Add enum integer values

--- Input Files/enum_listing2.py
+++ Input Files/enum_listing2.py
@@ -9,12 +9,13 @@ Created on Mon Sep 11 12:54:57 2017
 from dotnet.seamless import add_assemblies, load_assembly#, build_assembly
 
 # load PLEXOS assemblies
-plexos_path = 'C:/Program Files (x86)/Energy Exemplar/PLEXOS 7.4/'
+plexos_path = 'C:/Program Files (x86)/Energy Exemplar/PLEXOS 7.5/'
 add_assemblies(plexos_path)
 load_assembly('EEUTILITY')
 
 # .NET related imports
 from EEUTILITY.Enums import *
+from System import Enum
 
 fout = open('EEUTILITY_Enums.txt', 'w')
 
@@ -22,7 +23,7 @@ for t in type(ClassEnum).Assembly.GetTypes():
     if t.IsEnum:
         fout.write('{}\n'.format(t.Name))
         for en in t.GetEnumNames():
-            fout.write('\t{}\n'.format(en))
+            fout.write('\t{} = {}\n'.format(en, int(Enum.Parse(t, en))))
         fout.write('\n')
 
 fout.close()
