d64e102871cbc833e5b6c93aea727a3654431e94
sriharikakuppa <33642591+sriharikakuppa@users.noreply.github.com>
2019-10-10 17:46:23 -0700

    Create Edit_Execute_Query.py

---
+++ End To End/Edit_Execute_Query.py
@@ -0,0 +1,205 @@
+
+import os
+import sys, re
+import subprocess as sp
+import pandas as pd
+from datetime import datetime
+
+from shutil import copyfile
+
+# Python .NET interface
+from dotnet.seamless import add_assemblies, load_assembly#, build_assembly
+
+# load PLEXOS assemblies
+plexos_path = 'C:/Program Files (x86)/Energy Exemplar/PLEXOS 8.0/'
+add_assemblies(plexos_path)
+load_assembly('PLEXOS7_NET.Core')
+load_assembly('EEUTILITY')
+
+# .NET related imports
+from PLEXOS7_NET.Core import *
+from EEUTILITY.Enums import *
+
+if os.path.exists('rts_PLEXOS.xml'):
+
+    # delete the modified file if it already exists
+    if os.path.exists('rts2.xml'):
+        os.remove('rts2.xml')
+
+    # copy the PLEXOS input file
+    copyfile('rts_PLEXOS.xml', 'rts2.xml')
+    
+    # Create an object to store the input data
+    db = DatabaseCore()
+    db.Connection('rts2.xml')
+    
+    # Add a category
+    '''
+    Int32 AddCategory(
+    	ClassEnum nClassId,
+    	String strCategory
+    	)
+    '''
+    db.AddCategory(ClassEnum.Generator, 'API')
+    
+    # Add an object (and the System Membership)
+    '''
+    Int32 AddObject(
+    	String strName,
+    	ClassEnum nClassId,
+    	Boolean bAddSystemMembership,
+    	String strCategory[ = None],
+    	String strDescription[ = None]
+    	)
+    '''
+    db.AddObject('ApiGen', ClassEnum.Generator, True, 'API', 'Testing the API')
+    
+    # Add memberships
+    '''
+    Int32 AddMembership(
+    	CollectionEnum nCollectionId,
+    	String strParent,
+    	String strChild
+    	)
+    '''
+    db.AddMembership(CollectionEnum.GeneratorNodes, 'ApiGen', '101')    
+    db.AddMembership(CollectionEnum.GeneratorFuels, 'ApiGen', 'Coal/Steam')
+    db.AddMembership(CollectionEnum.ReserveGenerators, 'Spin Up', 'ApiGen')
+    
+    # Get the System.Generators membership ID for this new generator
+    '''
+    Int32 GetMembershipID(
+    	CollectionEnum nCollectionId,
+    	String strParent,
+    	String strChild
+    	)    
+    '''
+    mem_id = db.GetMembershipID(CollectionEnum.SystemGenerators, \
+                                'System', 'ApiGen')
+                                
+    # Add properties
+    '''
+    Int32 AddProperty(
+    	Int32 MembershipId,
+    	Int32 EnumId,
+    	Int32 BandId,
+    	Double Value,
+    	Object DateFrom,
+    	Object DateTo,
+    	Object Variable,
+    	Object DataFile,
+    	Object Pattern,
+    	Object Scenario,
+    	Object Action,
+    	PeriodEnum PeriodTypeId
+    	)
+     
+    Because of the number of parameters, we need
+        a. An alias for AddProperty
+        b. A tuple of parameter values
+        c. A call to the alias
+        
+    Also we need to obtain the EnumId for each property
+    that we intend to add
+    '''
+    # a. An alias for AddProperty
+    addProp = db.AddProperty[Int32,Int32,Int32,Double,Object, \
+                             Object,Object,Object,Object,Object, \
+                             Object,PeriodEnum]
+    
+    # b. A tuple of parameter values
+    params = (mem_id, int(SystemGeneratorsEnum.Units), \
+              1, 0.0, None, None, None, None, None, None, \
+              0, PeriodEnum.Interval)
+    
+    # c. A call to the alias
+    addProp.__invoke__(params)
+    
+    # save the data set
+    db.Close()
+
+#Execute the model 
+
+#Execute = db.GetModelsToExecute('rts2.xml',\
+                            #'Base',\
+                           # '')
+							
+
+def run_model(plexospath, filename, foldername, modelname):
+
+    # launch the model on the local desktop
+    # The \n argument is very important because it allows the PLEXOS
+    # engine to terminate after completing the simulation
+    sp.call([os.path.join(plexospath, 'PLEXOS64.exe'), filename, r'\n', r'\o', foldername, r'\m', modelname])
+
+def parse_logfile(pattern, foldername, modelname, linecount = 1):
+    
+    currentlines = 0
+    lines = []
+    regex = re.compile(pattern)
+    
+    for line in open(os.path.join(foldername, 'Model {} Solution'.format(modelname), 'Model ( {} ) Log.txt'.format(modelname))):
+        if len(regex.findall(line)) > 0:
+            currentlines = linecount
+            
+        if currentlines > 0:
+            lines.append(line)
+            currentlines -= 1
+            
+        if currentlines == 0 and len(lines) > 0:
+            retval = '\n'.join(lines)
+            lines = []
+            yield retval
+            
+def main():
+    run_model('C:\Program Files (x86)\Energy Exemplar\PLEXOS 8.0', 'rts2.xml', '.', 'Base')
+    for res in parse_logfile('ST Schedule Completed', '.', 'Base', 25):
+        print res
+       
+if __name__ == '__main__':
+    main()
+	
+#Query Results
+# Create a PLEXOS solution file object and load the solution
+sol = Solution()
+sol_file = 'Model Base Solution\Model Base Solution.zip' # replace with your solution file
+csv_file = 'Gens.csv'
+print 'line 24'
+if not os.path.exists(sol_file):
+    print 'No such file'
+    exit
+    
+sol.Connection(sol_file)
+
+'''
+Simple query: works similarly to PLEXOS Solution Viewer
+
+Solution.Query(phase, collection, parent, child, period, series, props)
+    phase -> SimulationPhaseEnum
+    collection -> CollectionEnum
+    parent -> the name of a parent object or ''
+    child -> the name of a child object or ''
+    period -> PeriodEnum
+    series -> SeriesTypeEnum
+    props -> a string containing an integer indicating the Property to query or ''
+returns a ADODB recordset... however, you don't *need* to worry about that...
+'''
+
+#to query Generation properties
+results = sol.QueryToCSV( \
+                    csv_file, False,
+                    SimulationPhaseEnum.STSchedule, \
+                    CollectionEnum.SystemGenerators, \
+                    '', \
+                    '', \
+                    PeriodEnum.Interval, \
+                    SeriesTypeEnum.Names, \
+                    '1')
+					
+print 'line 56'
+df = pd.read_csv(csv_file)
+print 'line 58'
+wb = pd.ExcelWriter('Gens.xlsx')
+df.to_excel(wb, 'Query') # 'Query' is the name of the worksheet
+
+wb.save()
