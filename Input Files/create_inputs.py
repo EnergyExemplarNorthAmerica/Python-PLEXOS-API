efc939fa3ce4ed5c099fcf6ee47f5a2e338901f9
Steven Broad <steven.broad@energyexemplar.com>
2020-03-20 11:29:43 -0700

    Updated create_inputs for pythonnet

--- Input Files/create_inputs.py
+++ Input Files/create_inputs.py
@@ -7,20 +7,17 @@ Created on Sat Sep 09 19:19:57 2017
 @author: Steven
 """
 
-import os
-
-# Python .NET interface
-from dotnet.seamless import add_assemblies, load_assembly#, build_assembly
+import os, sys, clr
 
 # load PLEXOS assemblies
-plexos_path = 'C:/Program Files (x86)/Energy Exemplar/PLEXOS 7.4/'
-add_assemblies(plexos_path)
-load_assembly('PLEXOS7_NET.Core')
-load_assembly('EEUTILITY')
+sys.path.append('C:/Program Files (x86)/Energy Exemplar/PLEXOS 8.1/')
+clr.AddReference('PLEXOS7_NET.Core')
+clr.AddReference('EEUTILITY')
 
 # .NET related imports
 from PLEXOS7_NET.Core import DatabaseCore
 from EEUTILITY.Enums import *
+from System import Enum
 
 def add_plexos_prop(db, parent_class_id, child_class_id, collection_id, \
                     parent_name, child_name, prop_name, prop_value, \
@@ -44,13 +41,6 @@ def add_plexos_prop(db, parent_class_id, child_class_id, collection_id, \
         else:
             db.AddObject(child_name, child_class_id, True, '', 'Added from Python')
             
-    # Add the property
-    # a. An alias for AddProperty
-    add_prop = db.AddProperty[Int32,Int32,Int32,Double,Object, \
-                             Object,Object,Object,Object,Object, \
-                             Object,PeriodEnum]
-
-    # b. A tuple of parameter values
     '''
     Int32 GetMembershipID(
     	CollectionEnum nCollectionId,
@@ -67,15 +57,15 @@ def add_plexos_prop(db, parent_class_id, child_class_id, collection_id, \
     	String strPropertyName
     	)
     '''
-    enum_id = db.PropertyName2EnumId(parent_class_id.ToString(), \
-                                    child_class_id.ToString(), \
-                                    child_class_id.ToString()+'s',prop_name)
-    params = (mem_id, enum_id, 1, prop_value, None, None, None, \
+    enum_id = db.PropertyName2EnumId( \
+        Enum.GetName(clr.GetClrType(ClassEnum), parent_class_id), \
+        Enum.GetName(clr.GetClrType(ClassEnum), child_class_id), \
+        Enum.GetName(clr.GetClrType(ClassEnum), child_class_id)+'s', \
+        prop_name)
+
+    db.AddProperty(mem_id, enum_id, 1, prop_value, None, None, None, \
               None, None, None, 0, PeriodEnum.Interval)
     
-    # c. A call to the alias
-    add_prop.__invoke__(params)
-
 # delete the modified file if it already exists
 if os.path.exists('new.xml'):
     os.remove('new.xml')
